9c5e5a50aa1dfea8922f7dca324e2ac5b205ad73
clajith <clajith@gmail.com>
2021-04-14 16:14:17 +0530

    Add project files.

---
+++ WindowsFormsAppWebspeed/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace WindowsFormsAppWebspeed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGuestNr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.Location = new System.Drawing.Point(404, 19);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(90, 35);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "Search";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Guest number : ";
+            // 
+            // txtGuestNr
+            // 
+            this.txtGuestNr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuestNr.Location = new System.Drawing.Point(165, 23);
+            this.txtGuestNr.Name = "txtGuestNr";
+            this.txtGuestNr.Size = new System.Drawing.Size(210, 27);
+            this.txtGuestNr.TabIndex = 2;
+            this.txtGuestNr.TextChanged += new System.EventHandler(this.txtGuestNr_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Country : ";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(166, 80);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(208, 31);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(166, 131);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(208, 31);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountry.Location = new System.Drawing.Point(166, 184);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(208, 31);
+            this.txtCountry.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 276);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGuestNr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGuestNr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtName;
+        private System.Windows.Forms.Label txtAddress;
+        private System.Windows.Forms.Label txtCountry;
+    }
+}
+
---
+++ WindowsFormsAppWebspeed/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsAppWebspeed
+{
+    public partial class Form1 : Form
+    {
+        private string guestNumber,guestName,guestAddress,guestCountry;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string urlString = "http://nlbavwsldev01/cgi-bin/cgiip.exe/WService=webspeedone/websample.p?guestnumber=" + guestNumber;
+            XmlTextReader reader = new XmlTextReader(urlString);
+            string CurrentElement =null;
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        {
+                            CurrentElement = reader.Name;
+                            break;
+                        }
+                    case XmlNodeType.Text:
+                        {
+                            switch (CurrentElement)
+                                {
+                                case "NAME":
+                                    guestName = reader.Value;
+                                    break;
+                                case "address1":
+                                    guestAddress = reader.Value;
+                                    break;
+                                case "country":
+                                    guestCountry = reader.Value;
+                                    break;                            
+                                }
+                            break;
+                        }
+            }
+            }
+            txtName.Text = guestName;
+            txtAddress.Text = guestAddress;
+            txtCountry.Text = guestCountry;
+        }
+
+        private void txtGuestNr_TextChanged(object sender, EventArgs e)
+        {
+            guestNumber = txtGuestNr.Text;
+        }
+    }
+}
